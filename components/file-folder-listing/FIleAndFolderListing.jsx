b5cd9f954674cd1a886e5badfb849844fafeea07
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-18 16:41:56 +0530

    - add btn UI created

--- components/file-folder-listing/FIleAndFolderListing.jsx
+++ components/file-folder-listing/FIleAndFolderListing.jsx
@@ -1,5 +1,16 @@
+import styles from "./styles.module.css";
+import { MdOutlineAdd } from "react-icons/md";
 const FIleAndFolderListing = () => {
-  return <section className="container">FIleAndFolderListing</section>;
+  return (
+    <section className="container">
+      <div className={`${styles.file_and_floder_list_container} w-100 d-flex`}>
+        <div
+          className={`${styles.add_btn} d-flex justify-content-center align-items-center`}>
+          <MdOutlineAdd className={`${styles.add_icon} display-md`} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FIleAndFolderListing;

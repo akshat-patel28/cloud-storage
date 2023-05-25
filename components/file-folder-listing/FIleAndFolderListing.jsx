0488dbc159e884ffecd6d11f9bda4d73376a4ffb
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-25 14:15:20 +0530

    - list mapping logic changed - file and floder will be maped as per cuurent selected path

--- components/file-folder-listing/FIleAndFolderListing.jsx
+++ components/file-folder-listing/FIleAndFolderListing.jsx
@@ -1,8 +1,10 @@
+"use client";
 import styles from "./styles.module.css";
 import { MdOutlineAdd } from "react-icons/md";
 import folderImage from "public/assets/images/folder.png";
 import fileImage from "public/assets/images/file.png";
 import Image from "next/image";
+import { useState } from "react";
 
 const filesAndFolders = [
   {
@@ -18,29 +20,41 @@ const filesAndFolders = [
     type: "file",
     name: "New File",
   },
+  {
+    path: "/newfolder",
+    id: "newFile",
+    type: "file",
+    name: "New File 2",
+  },
 ];
 const FIleAndFolderListing = () => {
+  const [currentPath, setCurrentPath] = useState("/");
   return (
     <section className="container">
       <div className={`${styles.file_and_floder_list_container} w-100 d-flex`}>
-        {filesAndFolders.map((item) =>
-          item.type === "folder" ? (
-            <div
-              className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
-              <Image src={folderImage} alt="folder" width={80} height={80} />
-              <p className={`${styles.margin_0} text-md`}>{item.name}</p>
-            </div>
-          ) : (
-            <div
-              className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
-              <Image src={fileImage} alt="file" width={80} height={80} />
-              <p
-                className={`${styles.margin_0} ${styles.margin_top_5} text-md`}>
-                {item.name}
-              </p>
-            </div>
-          )
-        )}
+        {filesAndFolders
+          .filter((item) => item.path === currentPath)
+          .map((item, index) =>
+            item.type === "folder" ? (
+              <div
+                onClick={() => setCurrentPath(item.internalPath)}
+                key={index}
+                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
+                <Image src={folderImage} alt="folder" width={80} height={80} />
+                <p className={`${styles.margin_0} text-md`}>{item.name}</p>
+              </div>
+            ) : (
+              <div
+                key={index}
+                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
+                <Image src={fileImage} alt="file" width={80} height={80} />
+                <p
+                  className={`${styles.margin_0} ${styles.margin_top_5} text-md`}>
+                  {item.name}
+                </p>
+              </div>
+            )
+          )}
         <div
           className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
           <div

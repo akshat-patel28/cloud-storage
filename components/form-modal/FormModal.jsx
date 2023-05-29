b8cb2abd1ab41233b94f14934fbe677c7dd2cce1
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-29 18:30:06 +0530

    - backdrop styles added in form modal component

--- components/form-modal/FormModal.jsx
+++ components/form-modal/FormModal.jsx
@@ -1,5 +1,14 @@
+import styles from "./styles.module.css";
+
 const FormModal = ({ showFormModal, setShowFormModal }) => {
-  return <div>FormModal</div>;
+  return (
+    <div
+      className={`${styles.modal_container} w-100 h-100 ${
+        showFormModal ? styles.active_modal : ""
+      }`}>
+      <span onClick={() => setShowFormModal(false)}>close</span>
+    </div>
+  );
 };
 
 export default FormModal;

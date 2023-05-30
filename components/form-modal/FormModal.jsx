b9d4c25063ab6f60ea29a5e402716b62a1233d42
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-30 12:18:37 +0530

    - Form UI created for to create new file and floder

--- components/form-modal/FormModal.jsx
+++ components/form-modal/FormModal.jsx
@@ -1,17 +1,116 @@
+import { Form, Formik } from "formik";
 import styles from "./styles.module.css";
 import { IoCloseCircle } from "react-icons/io5";
-
+import folderImage from "public/assets/images/folder.png";
+import fileImage from "public/assets/images/file.png";
+import Image from "next/image";
+import { MdOutlineError } from "react-icons/md";
+import * as Yup from "yup";
+const initialValues = {
+  fileType: "folder",
+  fileName: "",
+};
+const validationSchema = Yup.object().shape({
+  fileType: Yup.string().required(),
+  fileName: Yup.string().required("Required !!!"),
+});
 const FormModal = ({ showFormModal, setShowFormModal }) => {
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <div
       className={`${styles.modal_container} w-100 h-100 ${
         showFormModal ? styles.active_modal : ""
       } d-flex justify-content-center align-items-center`}>
-      <div className={`${styles.form_container} position-relative`}>
+      <div
+        className={`${styles.form_container} position-relative d-flex justify-content-center align-items-center`}>
         <IoCloseCircle
           className={`${styles.close_icon} position-absolute cursor-pointer `}
           onClick={() => setShowFormModal(false)}
         />
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}>
+          {({
+            handleSubmit,
+            handleBlur,
+            values,
+            handleChange,
+            setFieldValue,
+            errors,
+            touched,
+          }) => (
+            <Form onSubmit={handleSubmit} className="w-100">
+              <div className={`${styles.fields_container}`}>
+                <p className={`${styles.form_title} text-xl font-bold`}>
+                  Create New
+                </p>
+                <div>
+                  <div
+                    className={`${styles.file_type_fileds_container} d-flex justify-content-center`}>
+                    <div
+                      className={`${
+                        styles.imgae_container
+                      } d-flex justify-content-around align-items-center cursor-pointer ${
+                        values.fileType === "folder" ? styles.seleted_type : ""
+                      }`}
+                      onClick={() => setFieldValue("fileType", "folder")}>
+                      <Image
+                        src={folderImage}
+                        alt="folder"
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                    <div
+                      className={`${
+                        styles.imgae_container
+                      } d-flex justify-content-around align-items-center cursor-pointer ${
+                        values.fileType === "file" ? styles.seleted_type : ""
+                      }`}
+                      onClick={() => setFieldValue("fileType", "file")}>
+                      <Image
+                        src={fileImage}
+                        alt="file"
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="fileName"
+                    value={values.fileName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder={
+                      values.fileType === "file" ? "File Name" : "Folder Name"
+                    }
+                    className={`${styles.input_field} w-100`}
+                  />
+                  <div className={`${styles.field_error_container} text-sm`}>
+                    {errors.fileName && touched.fileName ? (
+                      <span className="d-flex align-items-center">
+                        <MdOutlineError />
+                        &nbsp;{errors.fileName}
+                      </span>
+                    ) : null}
+                  </div>
+                </div>
+
+                <button
+                  className={`${styles.form_button} cursor-pointer text-md font-bold`}
+                  type="submit">
+                  Create
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );

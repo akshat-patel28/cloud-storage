1a201776bbf8b30678110bac4aa40f84fda22019
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-30 10:28:51 +0530

    - form container created in form modal component - new close icon added - styles adedd for form container and close icon

--- components/form-modal/FormModal.jsx
+++ components/form-modal/FormModal.jsx
@@ -1,12 +1,18 @@
 import styles from "./styles.module.css";
+import { IoCloseCircle } from "react-icons/io5";
 
 const FormModal = ({ showFormModal, setShowFormModal }) => {
   return (
     <div
       className={`${styles.modal_container} w-100 h-100 ${
         showFormModal ? styles.active_modal : ""
-      }`}>
-      <span onClick={() => setShowFormModal(false)}>close</span>
+      } d-flex justify-content-center align-items-center`}>
+      <div className={`${styles.form_container} position-relative`}>
+        <IoCloseCircle
+          className={`${styles.close_icon} position-absolute cursor-pointer `}
+          onClick={() => setShowFormModal(false)}
+        />
+      </div>
     </div>
   );
 };

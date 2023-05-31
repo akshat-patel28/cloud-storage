f4c09b25b4912147f699b5aab2ac62bc6ef4721c
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-05-31 10:20:21 +0530

    - styles added to manage long file and floder name - will show name text as ellipsis after 1 line

--- components/file-folder-listing/FIleAndFolderListing.jsx
+++ components/file-folder-listing/FIleAndFolderListing.jsx
@@ -1,18 +1,18 @@
-import styles from "./styles.module.css";
-import { MdOutlineAdd } from "react-icons/md";
-import folderImage from "public/assets/images/folder.png";
-import fileImage from "public/assets/images/file.png";
-import Image from "next/image";
-import { useContext, useState } from "react";
-import { FileAndFolderContext } from "@/context/FileandFolderContext";
-import dynamic from "next/dynamic";
-const FormModal = dynamic(() => import("../form-modal/FormModal"), {
+import styles from './styles.module.css';
+import { MdOutlineAdd } from 'react-icons/md';
+import folderImage from 'public/assets/images/folder.png';
+import fileImage from 'public/assets/images/file.png';
+import Image from 'next/image';
+import { useContext, useState } from 'react';
+import { FileAndFolderContext } from '@/context/FileandFolderContext';
+import dynamic from 'next/dynamic';
+const FormModal = dynamic(() => import('../form-modal/FormModal'), {
   ssr: false,
   loading: () => <div></div>,
 });
 
 const FIleAndFolderListing = ({
-  currentPath = "",
+  currentPath = '',
   setCurrentPath = () => {},
 }) => {
   const { state } = useContext(FileAndFolderContext);
@@ -24,31 +24,40 @@ const FIleAndFolderListing = ({
         {filesAndFolders
           .filter((item) => item.path === currentPath)
           .map((item, index) =>
-            item.type === "folder" ? (
+            item.type === 'folder' ? (
               <div
                 onDoubleClick={() => setCurrentPath(item.internalPath)}
                 key={index}
-                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center cursor-pointer`}>
+                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center cursor-pointer`}
+              >
                 <Image src={folderImage} alt="folder" width={80} height={80} />
-                <p className={`${styles.margin_0} text-md`}>{item.name}</p>
+                <p
+                  className={`${styles.margin_0} text-md ${styles.file_folder_name_text}`}
+                >
+                  {item.name}
+                </p>
               </div>
             ) : (
               <div
                 key={index}
-                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
+                className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}
+              >
                 <Image src={fileImage} alt="file" width={80} height={80} />
                 <p
-                  className={`${styles.margin_0} ${styles.margin_top_5} text-md`}>
+                  className={`${styles.margin_0} ${styles.margin_top_5} text-md ${styles.file_folder_name_text}`}
+                >
                   {item.name}
                 </p>
               </div>
             )
           )}
         <div
-          className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}>
+          className={`${styles.file_or_folder_div} d-flex justify-content-center align-items-center`}
+        >
           <div
             className={`${styles.add_btn} d-flex justify-content-center align-items-center`}
-            onClick={() => setShowFormModal(true)}>
+            onClick={() => setShowFormModal(true)}
+          >
             <MdOutlineAdd className={`${styles.add_icon} display-md`} />
           </div>
         </div>

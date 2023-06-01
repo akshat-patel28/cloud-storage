a3413f08ed438619895b466034456a97e2267574
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-06-01 14:39:43 +0530

    - delete functionality added on detele action button

--- components/file-folder-listing/ActionMenu.jsx
+++ components/file-folder-listing/ActionMenu.jsx
@@ -1,12 +1,31 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
 import { CgRename } from 'react-icons/cg';
 import { FaRegTrashAlt } from 'react-icons/fa';
+import {
+  DELETE_FILE_OR_FOLDER_ACTION,
+  FileAndFolderContext,
+} from '@/context/FileandFolderContext';
 const ActionMenu = ({
   menuId,
   setShowRenameModal,
   openActionMenuId,
   setOpenActionMenuId,
+  currentSeletedFileOrFolder,
 }) => {
+  const { state, dispatch } = useContext(FileAndFolderContext);
+  const { filesAndFolders } = state;
+  console.log(filesAndFolders);
+  const deleteFileOrFolder = () => {
+    const filteredData = filesAndFolders.filter(
+      (item) =>
+        item.id !== menuId &&
+        !item.path.includes(
+          currentSeletedFileOrFolder.name.toLowerCase().replace(' ', '')
+        )
+    );
+    dispatch({ type: DELETE_FILE_OR_FOLDER_ACTION, payload: filteredData });
+  };
   if (menuId !== openActionMenuId) {
     return <></>;
   }
@@ -22,6 +41,7 @@ const ActionMenu = ({
         <CgRename /> <span>Rename</span>
       </div>
       <div
+        onClick={() => deleteFileOrFolder()}
         className={`${styles.action_label} ${styles.delete_action} w-100 d-flex  text-lg align-items-center cursor-pointer`}
       >
         <FaRegTrashAlt /> <span>Delete</span>

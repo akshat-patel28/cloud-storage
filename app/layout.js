3dfaf0f966f4670726cc2e440954117ac06a4f99
Akshat Patel <akshat.patel@globalvoxinc.com>
2023-06-12 10:01:43 +0530

    - basic setup done for redux toolkit

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,23 @@
-import { FileAndFolderProvider } from "@/context/FileandFolderContext";
-import "./globals.css";
-import { Inter } from "next/font/google";
+import { FileAndFolderProvider } from '@/context/FileandFolderContext';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Cloud",
-  description: "A Cloud Storage System",
+  title: 'Cloud',
+  description: 'A Cloud Storage System',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <FileAndFolderProvider>{children}</FileAndFolderProvider>
+        <Provider store={store}>
+          <FileAndFolderProvider>{children}</FileAndFolderProvider>
+        </Provider>
       </body>
     </html>
   );
